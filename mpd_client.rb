c9ca3de472a68730ed4d31fe0fa39588f3f8c3fc
Dan Barber <dan@new-bamboo.co.uk>
2013-08-30 10:50:45 +0100

    Now list artists, albums and songs.

--- mpd_client.rb
+++ mpd_client.rb
@@ -1,14 +1,49 @@
 require 'sinatra'
+require "sinatra/namespace"
 require 'json'
 require 'ruby-mpd'
+require 'cgi'
 
 class MPDClient < Sinatra::Base
-  before do
-    @mpd = MPD.new
-    @mpd.connect
+  register Sinatra::Namespace
+
+  namespace '/api' do
+
+    get '/albums' do
+      JSON mpd.albums
+    end
+
+    get '/artists/:artist' do
+      JSON mpd.albums(CGI.unescape(params[:artist]))
+    end
+
+    get '/albums/:album' do
+      JSON get_songs_by_album(CGI.unescape(params[:album]))
+    end
+
+    get '/artists/:artist/:album' do
+      JSON get_songs_by_album(CGI.unescape(params[:album]))
+    end
+
+    get '/artists' do
+      JSON mpd.artists
+    end
+
   end
 
-  get '/api/albums' do
-    @mpd.albums
+  private
+
+  def get_songs_by_album(album)
+    mpd.search(:album, album).map do |song|
+      { tracknumber: song.track,
+        title: song.title }
+    end
   end
+
+  def mpd
+    @mpd ||= MPD.new
+    @mpd.connect unless @mpd.connected?
+    @mpd
+  end
+
 end

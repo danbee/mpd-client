73e4e55d924e791c15d92c2b493dfb0415c435c4
Dan Barber <dan@new-bamboo.co.uk>
2013-10-17 18:21:17 +0100

    Add controls and current playing style.

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,3 +5,13 @@ App.QueueRoute = Ember.Route.extend({
     return Ember.$.getJSON('/api/queue');
   }
 });
+
+App.ControlsController = Ember.Controller.extend({
+  actions: {
+    previous: function() { return Ember.$.ajax('/api/control/previous', { type: 'PUT' }); },
+    play:     function() { return Ember.$.ajax('/api/control/play', { type: 'PUT' }); },
+    pause:    function() { return Ember.$.ajax('/api/control/pause', { type: 'PUT' }); },
+    stop:     function() { return Ember.$.ajax('/api/control/stop', { type: 'PUT' }); },
+    next:     function() { return Ember.$.ajax('/api/control/next', { type: 'PUT' }); }
+  }
+});
--- models/song.rb
+++ models/song.rb
@@ -1,15 +1,17 @@
 require './models/mpd_connection'
 
-class Song < Struct.new(:artist, :album, :title)
+class Song < Struct.new(:artist, :album, :title, :current)
 
-  def initialize(song)
+  def initialize(song, current: false)
     @song = song
     self.artist = song.artist
     self.album = song.album
     self.title  = song.title
+    self.current = current
   end
 
   def self.queue
-    MPDConnection.mpd.queue.map { |song| self.new(song) }
+    current_song = MPDConnection.mpd.status[:songid]
+    MPDConnection.mpd.queue.map { |song| self.new(song, current: (song.id == current_song)) }
   end
 end

71e689a880229583b48bd13ae876230d93f7e78d
Dan Barber <dan@new-bamboo.co.uk>
2013-10-18 13:30:28 +0100

    Change 'current' to 'playing'.

--- models/song.rb
+++ models/song.rb
@@ -1,17 +1,18 @@
 require './models/mpd_connection'
 
-class Song < Struct.new(:artist, :album, :title, :current)
+class Song < Struct.new(:id, :artist, :album, :title, :playing)
 
-  def initialize(song, current: false)
+  def initialize(song, playing: false)
     @song = song
+    self.id = song.songid
     self.artist = song.artist
     self.album = song.album
     self.title  = song.title
-    self.current = current
+    self.playing = playing
   end
 
   def self.queue
     current_song = MPDConnection.mpd.status[:songid]
-    MPDConnection.mpd.queue.map { |song| self.new(song, current: (song.id == current_song)) }
+    MPDConnection.mpd.queue.map { |song| self.new(song, playing: (song.id == current_song)) }
   end
 end

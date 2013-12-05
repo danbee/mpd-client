280455ca4e065c4e7addd391185f17f3668cedf1
Dan Barber <dan@new-bamboo.co.uk>
2013-12-05 16:40:51 +0000

    Make play/pause button dynamic.

--- assets/js/controls/transport.js
+++ assets/js/controls/transport.js
@@ -2,7 +2,7 @@ var Transport = can.Control.extend({
 
   init: function(element, options) {
     this.status = options.status;
-    element.html(can.view('views/transport.ejs'));
+    element.html(can.view('views/transport.ejs', { status: this.status }));
   },
 
   updateStatus: function(status) {
@@ -23,6 +23,7 @@ var Transport = can.Control.extend({
   'button click': function(element, event) {
     var command = $(element).data('command');
     this.sendCommand(command);
+    $(element).blur();
   }
 
 });

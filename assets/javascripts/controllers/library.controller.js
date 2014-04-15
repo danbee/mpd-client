87bc4a434a1b8853b64669fc890d49a5878479c5
Dan Barber <dan@new-bamboo.co.uk>
2014-04-15 15:32:17 +0100

    Can show and hide library.

--- assets/javascripts/controllers/library.controller.js
+++ assets/javascripts/controllers/library.controller.js
@@ -1,2 +1,7 @@
 mpdClient.controller('library', function ($scope, api) {
+  $scope.show = false
+
+  $scope.$on('library:show', function () { $scope.show = true })
+
+  $scope.hide = function () { $scope.show = false }
 })
---
+++ assets/javascripts/controllers/mpd-client.controller.js
@@ -0,0 +1,5 @@
+mpdClient.controller('mpd-client', function ($rootScope, $scope) {
+  $scope.showLibrary = function () {
+    $scope.$broadcast('library:show')
+  }
+})

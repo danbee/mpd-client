8c2412ca985e903f6caac30f64a192326cb406a3
Dan Barber <dan@new-bamboo.co.uk>
2014-04-15 18:16:11 +0100

    Add root pane.

--- assets/javascripts/controllers/library.controller.js
+++ assets/javascripts/controllers/library.controller.js
@@ -4,4 +4,25 @@ mpdClient.controller('library', function ($scope, api) {
   $scope.$on('library:show', function () { $scope.show = true })
 
   $scope.hide = function () { $scope.show = false }
+
+  var rootPane = {
+    title: 'Library',
+    entries: [
+      { label: 'Artists', path: '/artists' },
+      { label: 'Albums', path: '/albums' },
+      { label: 'Songs', path: '/songs' }
+    ]
+  }
+
+  $scope.panes = [rootPane]
+
+  $scope.currentPaneIndex = 0
+
+  $scope.currentPane = function () {
+    return $scope.panes[$scope.currentPaneIndex]
+  }
+
+  $scope.newPane = function (path, params) {
+    console.log(path)
+  }
 })
--- assets/javascripts/controllers/mpd-client.controller.js
+++ assets/javascripts/controllers/mpd-client.controller.js
@@ -1,3 +1,5 @@
 mpdClient.controller('mpd-client', function ($rootScope, $scope) {
   $scope.showLibrary = function () { $scope.$broadcast('library:show') }
+
+  $scope.currentPanelTemplate = 'panels/root.html'
 })

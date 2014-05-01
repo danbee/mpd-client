ed34cf4c9104bcdbf982dd722231b88ea2eb8f6b
Dan Barber <dan@new-bamboo.co.uk>
2014-05-01 14:00:30 +0100

    Implement back button.

--- assets/javascripts/controllers/library.controller.js
+++ assets/javascripts/controllers/library.controller.js
@@ -1,11 +1,14 @@
 mpdClient.controller('library', function ($scope, api) {
   $scope.show = false
 
+  $scope.title = 'Library'
+
   $scope.$on('library:show', function () { $scope.show = true })
 
   $scope.hide = function () { $scope.show = false }
 
   var rootPane = {
+    path: '/root',
     title: 'Library',
     entries: [
       { label: 'Artists', path: '/artists' },
@@ -26,12 +29,23 @@ mpdClient.controller('library', function ($scope, api) {
 
   $scope.newPane = function (path, params, queryParams) {
     $scope.panes.push({
+      path: path,
       title: params.title,
       entries: api.getItems(path).query(queryParams)
     })
 
     $scope.currentPaneIndex += 1
-
+    $scope.title = params.title
     $scope.currentPanelTemplate = 'panels' + path + '.html'
+  }
+
+  $scope.back = function() {
+    lastPane = $scope.panes.pop()
+
+    currentPane = $scope.panes[$scope.panes.length - 1]
+
+    $scope.currentPaneIndex -= 1
+    $scope.title = currentPane.title
+    $scope.currentPanelTemplate = 'panels' + currentPane.path + '.html'
   }
 })
